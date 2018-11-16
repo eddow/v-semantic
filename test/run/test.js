f0999d6819e0429bcb52910ce5173e4e8509a84d
fmdm <eeddow@gmail.com>
2018-11-16 23:47:48 +0200

    code finally mirror

--- test/run/test.js
+++ test/run/test.js
@@ -5913,6 +5913,320 @@ function escapeJsonPtr(str) {
 
 /***/ }),
 
+/***/ "../../node_modules/codemirror/addon/fold/foldcode.js":
+/*!************************************************************************!*\
+  !*** C:/dev/v-semantic/node_modules/codemirror/addon/fold/foldcode.js ***!
+  \************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: https://codemirror.net/LICENSE
+
+(function(mod) {
+  if (true) // CommonJS
+    mod(__webpack_require__(/*! ../../lib/codemirror */ "../../node_modules/codemirror/lib/codemirror.js"));
+  else {}
+})(function(CodeMirror) {
+  "use strict";
+
+  function doFold(cm, pos, options, force) {
+    if (options && options.call) {
+      var finder = options;
+      options = null;
+    } else {
+      var finder = getOption(cm, options, "rangeFinder");
+    }
+    if (typeof pos == "number") pos = CodeMirror.Pos(pos, 0);
+    var minSize = getOption(cm, options, "minFoldSize");
+
+    function getRange(allowFolded) {
+      var range = finder(cm, pos);
+      if (!range || range.to.line - range.from.line < minSize) return null;
+      var marks = cm.findMarksAt(range.from);
+      for (var i = 0; i < marks.length; ++i) {
+        if (marks[i].__isFold && force !== "fold") {
+          if (!allowFolded) return null;
+          range.cleared = true;
+          marks[i].clear();
+        }
+      }
+      return range;
+    }
+
+    var range = getRange(true);
+    if (getOption(cm, options, "scanUp")) while (!range && pos.line > cm.firstLine()) {
+      pos = CodeMirror.Pos(pos.line - 1, 0);
+      range = getRange(false);
+    }
+    if (!range || range.cleared || force === "unfold") return;
+
+    var myWidget = makeWidget(cm, options);
+    CodeMirror.on(myWidget, "mousedown", function(e) {
+      myRange.clear();
+      CodeMirror.e_preventDefault(e);
+    });
+    var myRange = cm.markText(range.from, range.to, {
+      replacedWith: myWidget,
+      clearOnEnter: getOption(cm, options, "clearOnEnter"),
+      __isFold: true
+    });
+    myRange.on("clear", function(from, to) {
+      CodeMirror.signal(cm, "unfold", cm, from, to);
+    });
+    CodeMirror.signal(cm, "fold", cm, range.from, range.to);
+  }
+
+  function makeWidget(cm, options) {
+    var widget = getOption(cm, options, "widget");
+    if (typeof widget == "string") {
+      var text = document.createTextNode(widget);
+      widget = document.createElement("span");
+      widget.appendChild(text);
+      widget.className = "CodeMirror-foldmarker";
+    } else if (widget) {
+      widget = widget.cloneNode(true)
+    }
+    return widget;
+  }
+
+  // Clumsy backwards-compatible interface
+  CodeMirror.newFoldFunction = function(rangeFinder, widget) {
+    return function(cm, pos) { doFold(cm, pos, {rangeFinder: rangeFinder, widget: widget}); };
+  };
+
+  // New-style interface
+  CodeMirror.defineExtension("foldCode", function(pos, options, force) {
+    doFold(this, pos, options, force);
+  });
+
+  CodeMirror.defineExtension("isFolded", function(pos) {
+    var marks = this.findMarksAt(pos);
+    for (var i = 0; i < marks.length; ++i)
+      if (marks[i].__isFold) return true;
+  });
+
+  CodeMirror.commands.toggleFold = function(cm) {
+    cm.foldCode(cm.getCursor());
+  };
+  CodeMirror.commands.fold = function(cm) {
+    cm.foldCode(cm.getCursor(), null, "fold");
+  };
+  CodeMirror.commands.unfold = function(cm) {
+    cm.foldCode(cm.getCursor(), null, "unfold");
+  };
+  CodeMirror.commands.foldAll = function(cm) {
+    cm.operation(function() {
+      for (var i = cm.firstLine(), e = cm.lastLine(); i <= e; i++)
+        cm.foldCode(CodeMirror.Pos(i, 0), null, "fold");
+    });
+  };
+  CodeMirror.commands.unfoldAll = function(cm) {
+    cm.operation(function() {
+      for (var i = cm.firstLine(), e = cm.lastLine(); i <= e; i++)
+        cm.foldCode(CodeMirror.Pos(i, 0), null, "unfold");
+    });
+  };
+
+  CodeMirror.registerHelper("fold", "combine", function() {
+    var funcs = Array.prototype.slice.call(arguments, 0);
+    return function(cm, start) {
+      for (var i = 0; i < funcs.length; ++i) {
+        var found = funcs[i](cm, start);
+        if (found) return found;
+      }
+    };
+  });
+
+  CodeMirror.registerHelper("fold", "auto", function(cm, start) {
+    var helpers = cm.getHelpers(start, "fold");
+    for (var i = 0; i < helpers.length; i++) {
+      var cur = helpers[i](cm, start);
+      if (cur) return cur;
+    }
+  });
+
+  var defaultOptions = {
+    rangeFinder: CodeMirror.fold.auto,
+    widget: "\u2194",
+    minFoldSize: 0,
+    scanUp: false,
+    clearOnEnter: true
+  };
+
+  CodeMirror.defineOption("foldOptions", null);
+
+  function getOption(cm, options, name) {
+    if (options && options[name] !== undefined)
+      return options[name];
+    var editorOptions = cm.options.foldOptions;
+    if (editorOptions && editorOptions[name] !== undefined)
+      return editorOptions[name];
+    return defaultOptions[name];
+  }
+
+  CodeMirror.defineExtension("foldOption", function(options, name) {
+    return getOption(this, options, name);
+  });
+});
+
+
+/***/ }),
+
+/***/ "../../node_modules/codemirror/addon/fold/foldgutter.js":
+/*!**************************************************************************!*\
+  !*** C:/dev/v-semantic/node_modules/codemirror/addon/fold/foldgutter.js ***!
+  \**************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: https://codemirror.net/LICENSE
+
+(function(mod) {
+  if (true) // CommonJS
+    mod(__webpack_require__(/*! ../../lib/codemirror */ "../../node_modules/codemirror/lib/codemirror.js"), __webpack_require__(/*! ./foldcode */ "../../node_modules/codemirror/addon/fold/foldcode.js"));
+  else {}
+})(function(CodeMirror) {
+  "use strict";
+
+  CodeMirror.defineOption("foldGutter", false, function(cm, val, old) {
+    if (old && old != CodeMirror.Init) {
+      cm.clearGutter(cm.state.foldGutter.options.gutter);
+      cm.state.foldGutter = null;
+      cm.off("gutterClick", onGutterClick);
+      cm.off("change", onChange);
+      cm.off("viewportChange", onViewportChange);
+      cm.off("fold", onFold);
+      cm.off("unfold", onFold);
+      cm.off("swapDoc", onChange);
+    }
+    if (val) {
+      cm.state.foldGutter = new State(parseOptions(val));
+      updateInViewport(cm);
+      cm.on("gutterClick", onGutterClick);
+      cm.on("change", onChange);
+      cm.on("viewportChange", onViewportChange);
+      cm.on("fold", onFold);
+      cm.on("unfold", onFold);
+      cm.on("swapDoc", onChange);
+    }
+  });
+
+  var Pos = CodeMirror.Pos;
+
+  function State(options) {
+    this.options = options;
+    this.from = this.to = 0;
+  }
+
+  function parseOptions(opts) {
+    if (opts === true) opts = {};
+    if (opts.gutter == null) opts.gutter = "CodeMirror-foldgutter";
+    if (opts.indicatorOpen == null) opts.indicatorOpen = "CodeMirror-foldgutter-open";
+    if (opts.indicatorFolded == null) opts.indicatorFolded = "CodeMirror-foldgutter-folded";
+    return opts;
+  }
+
+  function isFolded(cm, line) {
+    var marks = cm.findMarks(Pos(line, 0), Pos(line + 1, 0));
+    for (var i = 0; i < marks.length; ++i)
+      if (marks[i].__isFold && marks[i].find().from.line == line) return marks[i];
+  }
+
+  function marker(spec) {
+    if (typeof spec == "string") {
+      var elt = document.createElement("div");
+      elt.className = spec + " CodeMirror-guttermarker-subtle";
+      return elt;
+    } else {
+      return spec.cloneNode(true);
+    }
+  }
+
+  function updateFoldInfo(cm, from, to) {
+    var opts = cm.state.foldGutter.options, cur = from;
+    var minSize = cm.foldOption(opts, "minFoldSize");
+    var func = cm.foldOption(opts, "rangeFinder");
+    cm.eachLine(from, to, function(line) {
+      var mark = null;
+      if (isFolded(cm, cur)) {
+        mark = marker(opts.indicatorFolded);
+      } else {
+        var pos = Pos(cur, 0);
+        var range = func && func(cm, pos);
+        if (range && range.to.line - range.from.line >= minSize)
+          mark = marker(opts.indicatorOpen);
+      }
+      cm.setGutterMarker(line, opts.gutter, mark);
+      ++cur;
+    });
+  }
+
+  function updateInViewport(cm) {
+    var vp = cm.getViewport(), state = cm.state.foldGutter;
+    if (!state) return;
+    cm.operation(function() {
+      updateFoldInfo(cm, vp.from, vp.to);
+    });
+    state.from = vp.from; state.to = vp.to;
+  }
+
+  function onGutterClick(cm, line, gutter) {
+    var state = cm.state.foldGutter;
+    if (!state) return;
+    var opts = state.options;
+    if (gutter != opts.gutter) return;
+    var folded = isFolded(cm, line);
+    if (folded) folded.clear();
+    else cm.foldCode(Pos(line, 0), opts.rangeFinder);
+  }
+
+  function onChange(cm) {
+    var state = cm.state.foldGutter;
+    if (!state) return;
+    var opts = state.options;
+    state.from = state.to = 0;
+    clearTimeout(state.changeUpdate);
+    state.changeUpdate = setTimeout(function() { updateInViewport(cm); }, opts.foldOnChangeTimeSpan || 600);
+  }
+
+  function onViewportChange(cm) {
+    var state = cm.state.foldGutter;
+    if (!state) return;
+    var opts = state.options;
+    clearTimeout(state.changeUpdate);
+    state.changeUpdate = setTimeout(function() {
+      var vp = cm.getViewport();
+      if (state.from == state.to || vp.from - state.to > 20 || state.from - vp.to > 20) {
+        updateInViewport(cm);
+      } else {
+        cm.operation(function() {
+          if (vp.from < state.from) {
+            updateFoldInfo(cm, vp.from, state.from);
+            state.from = vp.from;
+          }
+          if (vp.to > state.to) {
+            updateFoldInfo(cm, state.to, vp.to);
+            state.to = vp.to;
+          }
+        });
+      }
+    }, opts.updateViewportTimeSpan || 400);
+  }
+
+  function onFold(cm, from) {
+    var state = cm.state.foldGutter;
+    if (!state) return;
+    var line = from.line;
+    if (line >= state.from && line < state.to)
+      updateFoldInfo(cm, line, line + 1);
+  }
+});
+
+
+/***/ }),
+
 /***/ "../../node_modules/codemirror/addon/mode/multiplex.js":
 /*!*************************************************************************!*\
   !*** C:/dev/v-semantic/node_modules/codemirror/addon/mode/multiplex.js ***!
@@ -80003,6 +80317,19 @@ var vue_router_1 = __webpack_require__(/*! vue-router */ "../../node_modules/vue
 vue_1.default.use(vue_router_1.default);
 var VueCodeMirror = __webpack_require__(/*! vue-codemirror */ "../../node_modules/vue-codemirror/dist/vue-codemirror.js");
 vue_1.default.use(VueCodeMirror);
+__webpack_require__(/*! codemirror/lib/codemirror.css */ "../../node_modules/codemirror/lib/codemirror.css");
+__webpack_require__(/*! codemirror/theme/base16-dark.css */ "../../node_modules/codemirror/theme/base16-dark.css");
+__webpack_require__(/*! codemirror/mode/meta */ "../../node_modules/codemirror/mode/meta.js");
+__webpack_require__(/*! codemirror/addon/mode/overlay */ "../../node_modules/codemirror/addon/mode/overlay.js");
+__webpack_require__(/*! codemirror/addon/mode/simple */ "../../node_modules/codemirror/addon/mode/simple.js");
+__webpack_require__(/*! codemirror/addon/selection/selection-pointer */ "../../node_modules/codemirror/addon/selection/selection-pointer.js");
+__webpack_require__(/*! codemirror/addon/fold/foldgutter */ "../../node_modules/codemirror/addon/fold/foldgutter.js");
+__webpack_require__(/*! codemirror/mode/xml/xml */ "../../node_modules/codemirror/mode/xml/xml.js");
+__webpack_require__(/*! codemirror/mode/javascript/javascript */ "../../node_modules/codemirror/mode/javascript/javascript.js");
+__webpack_require__(/*! codemirror/mode/css/css */ "../../node_modules/codemirror/mode/css/css.js");
+__webpack_require__(/*! codemirror/mode/handlebars/handlebars */ "../../node_modules/codemirror/mode/handlebars/handlebars.js");
+__webpack_require__(/*! codemirror/mode/htmlmixed/htmlmixed */ "../../node_modules/codemirror/mode/htmlmixed/htmlmixed.js");
+__webpack_require__(/*! codemirror/mode/vue/vue */ "../../node_modules/codemirror/mode/vue/vue.js");
 var app_vue_1 = __webpack_require__(/*! ./app.vue */ "./app.vue");
 var routes_1 = __webpack_require__(/*! ./routes */ "./routes.ts");
 new app_vue_1.default({
@@ -80012,18 +80339,6 @@ new app_vue_1.default({
     }),
     el: 'app'
 });
-__webpack_require__(/*! codemirror/lib/codemirror.css */ "../../node_modules/codemirror/lib/codemirror.css");
-__webpack_require__(/*! codemirror/theme/base16-dark.css */ "../../node_modules/codemirror/theme/base16-dark.css");
-__webpack_require__(/*! codemirror/mode/meta */ "../../node_modules/codemirror/mode/meta.js");
-__webpack_require__(/*! codemirror/addon/mode/overlay */ "../../node_modules/codemirror/addon/mode/overlay.js");
-__webpack_require__(/*! codemirror/addon/mode/simple */ "../../node_modules/codemirror/addon/mode/simple.js");
-__webpack_require__(/*! codemirror/addon/selection/selection-pointer */ "../../node_modules/codemirror/addon/selection/selection-pointer.js");
-__webpack_require__(/*! codemirror/mode/xml/xml */ "../../node_modules/codemirror/mode/xml/xml.js");
-__webpack_require__(/*! codemirror/mode/javascript/javascript */ "../../node_modules/codemirror/mode/javascript/javascript.js");
-__webpack_require__(/*! codemirror/mode/css/css */ "../../node_modules/codemirror/mode/css/css.js");
-__webpack_require__(/*! codemirror/mode/handlebars/handlebars */ "../../node_modules/codemirror/mode/handlebars/handlebars.js");
-__webpack_require__(/*! codemirror/mode/htmlmixed/htmlmixed */ "../../node_modules/codemirror/mode/htmlmixed/htmlmixed.js");
-__webpack_require__(/*! codemirror/mode/vue/vue */ "../../node_modules/codemirror/mode/vue/vue.js");
 
 
 /***/ }),
--- test/src/index.ts
+++ test/src/index.ts
@@ -8,6 +8,21 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 import * as VueCodeMirror from 'vue-codemirror'
 Vue.use(VueCodeMirror)
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-dark.css'
+import 'codemirror/mode/meta'
+import 'codemirror/addon/mode/overlay'
+import 'codemirror/addon/mode/simple'
+import 'codemirror/addon/selection/selection-pointer'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/handlebars/handlebars'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/vue/vue'
+
 import App from './app.vue'
 import routes from './routes'
 
@@ -18,16 +33,3 @@ new App({
 	}),
 	el: 'app'
 });
-
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/base16-dark.css'
-import 'codemirror/mode/meta'
-import 'codemirror/addon/mode/overlay'
-import 'codemirror/addon/mode/simple'
-import 'codemirror/addon/selection/selection-pointer'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import 'codemirror/mode/handlebars/handlebars'
-import 'codemirror/mode/htmlmixed/htmlmixed'
-import 'codemirror/mode/vue/vue'
